2e38f1010f2b646055ee44cef0ae73b1b74bd44a
ballalssica <ssicanara@naver.com>
2024-11-14 18:27:57 +0900

    chore: SelectedStation 변경
    위젯 아래쪽 선택됨/선택가능 디자인 추가

--- lib/seat/widgets/selected_station.dart
+++ lib/seat/widgets/selected_station.dart
@@ -14,39 +14,70 @@ class SelectedStation extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: Column(
         children: [
-          Expanded(
-            child: Text(
-              departureStation,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.purple,
-                fontWeight: FontWeight.bold,
+          //선택된 역 표시
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Text(
+                  departureStation,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.purple,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
-          ),
-          SizedBox(width: 8),
-          Icon(
-            Icons.arrow_circle_right_outlined,
-            color: Colors.black,
-            size: 30, // 아이콘 크기 조절
-          ),
-          SizedBox(width: 8),
-          Expanded(
-            child: Text(
-              arrivalStation,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.purple,
-                fontWeight: FontWeight.bold,
+              SizedBox(width: 8),
+              Icon(
+                Icons.arrow_circle_right_outlined,
+                color: Colors.black,
+                size: 30, // 아이콘 크기 조절
               ),
-            ),
+              SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  arrivalStation,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.purple,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
           ),
+          SizedBox(height: 10,),
+          // 선택좌석 표시 가이드
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(5)
+                ),
+              ),
+              Text('선택됨'),
+              SizedBox(width: 10,),
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(5)
+                ),
+              ),
+              Text('선택가능')
+            ],
+          )
         ],
       ),
     );
