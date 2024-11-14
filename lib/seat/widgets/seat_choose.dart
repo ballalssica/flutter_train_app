63429d2848c1faa7c4c05f01a0cbadb41e681861
ballalssica <ssicanara@naver.com>
2024-11-14 21:46:30 +0900

    chore: SeatBox 레이아웃작업
    SeatBox 레이아웃 디자인 완료

--- lib/seat/widgets/seat_choose.dart
+++ lib/seat/widgets/seat_choose.dart
@@ -13,8 +13,47 @@ class SeatChoose extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SeatChooseHeader(), 
-                // 아래에 다른 위젯들을 추가할 수 있음
+                SeatBoxHeader(), 
+                SizedBox(height: 10,),
+                row(1),
+                SizedBox(height: 5,),
+                row(2),
+                SizedBox(height: 5,),
+                row(3),
+                SizedBox(height: 5,),
+                row(4),
+                SizedBox(height: 5,),
+                row(5),
+                SizedBox(height: 5,),
+                row(6),
+                SizedBox(height: 5,),
+                row(7),
+                SizedBox(height: 5,),
+                row(8),
+                SizedBox(height: 5,),
+                row(9),
+                SizedBox(height: 5,),
+                row(10),
+                SizedBox(height: 5,),
+                row(11),
+                SizedBox(height: 5,),
+                row(12),
+                SizedBox(height: 5,),
+                row(13),
+                SizedBox(height: 5,),
+                row(14),
+                SizedBox(height: 5,),
+                row(15),
+                SizedBox(height: 5,),
+                row(16),
+                SizedBox(height: 5,),
+                row(17),
+                SizedBox(height: 5,),
+                row(18),
+                SizedBox(height: 5,),
+                row(19),
+                SizedBox(height: 5,),
+                row(20),
               ],
             ),
           ),
@@ -22,10 +61,54 @@ class SeatChoose extends StatelessWidget {
       ),
     );
   }
+
+  Widget row(int rowNum) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    seat(),
+                    SizedBox(width: 10,),
+                    seat(),
+                    SizedBox(width: 10,),
+                    Container(
+                      width: 40,
+                      height: 40,
+                      child: Center(
+                        child: Text(
+                          '$rowNum',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                      ),
+                    SizedBox(width: 10,),
+                    seat(),
+                    SizedBox(width: 10,),
+                    seat(),
+                  ],
+                ),
+    );
+  }
 }
 
-// 좌석선택 헤더
-class SeatChooseHeader extends StatelessWidget {
+
+// 좌석위젯
+Widget seat() {
+  return Container(
+    width: 40,
+    height: 40,
+    decoration: BoxDecoration(
+      color: Colors.black12,
+      borderRadius: BorderRadius.circular(10),
+    ),
+  );
+}
+
+// SeatBoxHeader
+class SeatBoxHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -63,3 +146,6 @@ class SeatChooseHeader extends StatelessWidget {
     );
   }
 }
+
+
+
--- lib/seat/widgets/selected_station.dart
+++ lib/seat/widgets/selected_station.dart
@@ -72,7 +72,7 @@ class SelectedStation extends StatelessWidget{
                 width: 20,
                 height: 20,
                 decoration: BoxDecoration(
-                  color: Colors.grey,
+                  color: Colors.black12,
                   borderRadius: BorderRadius.circular(5)
                 ),
               ),
