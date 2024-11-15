af8884e2a36a9df8617ce262cb846504e3b6a42b
ballalssica <ssicanara@naver.com>
2024-11-15 09:05:23 +0900

    docs: 예매하기버튼추가
    SeatPage 예매하기버튼추가

--- lib/seat/widgets/seat_choose_button.dart
+++ lib/seat/widgets/seat_choose_button.dart
@@ -9,22 +9,28 @@ class SeatChooseButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        // 버튼 클릭 시 수행할 작업
-      },
-      style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-        backgroundColor: Colors.purple,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10), // 둥근 모서리 설정
-        ),
-      ),
-      child: Text(
-        '좌석 선택',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 16,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () {
+            // 버튼 클릭 시 수행할 작업
+          },
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+            backgroundColor: Colors.purple,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10), // 둥근 모서리 설정
+            ),
+          ),
+          child: Text(
+            '예매 하기',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
         ),
       ),
     );
